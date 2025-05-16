53c5d6334cc3cf44c17615cbb72ca246168accae
Dario <dariolopezcastillejo@alumnos.ilerna.com>
2025-05-16 12:37:11 +0200

    En este archivo he cambiado los nombres de las variables a jugador1 y jugqdor2

--- reto7/java/Juego.java
+++ reto7/java/Juego.java
@@ -2,16 +2,16 @@ package reto7.java;
 
 public class Juego {
     public static void main(String[] args) {
-        Jugador j1 = new Jugador("Arthas", 100, 30, 10);
-        Jugador j2 = new Jugador("Thrall", 100, 25, 15);
+        Jugador jugador1 = new Jugador("Arthas", 100, 30, 10);
+        Jugador jugador2 = new Jugador("Thrall", 100, 25, 15);
 
-        if (j1.ataque > j2.defensa) {
-            j2.vida -= (j1.ataque - j2.defensa);
+        if (jugador1.ataque > jugador2.defensa) {
+            jugador2.vida -= (jugador1.ataque - jugador2.defensa);
         } else {
-            j1.vida -= 5;
+            jugador1.vida -= 5;
         }
 
-        j1.mostrar();
-        j2.mostrar();
+        jugador1.mostrar();
+        jugador2.mostrar();
     }
 }
