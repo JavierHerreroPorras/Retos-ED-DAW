d2058a028efbd6a93bd8826f958f493dd5bfd6c2
Dario <dariolopezcastillejo@alumnos.ilerna.com>
2025-05-16 12:42:40 +0200

    En este archivo he añadido getters y setters

--- reto7/java/Jugador.java
+++ reto7/java/Jugador.java
@@ -1,23 +1,65 @@
 package reto7.java;
 
 public class Jugador {
-    public String nombre;
-    public int vida;
-    public int ataque;
-    public int defensa;
+    private String nombre;
+    private int vida;
+    private int ataque;
+    private int defensa;
 
-    public Jugador(String n, int v, int a, int d) {
-        nombre = n;
-        vida = v;
-        ataque = a;
-        defensa = d;
+    public Jugador(String nombre, int vida, int ataque, int defensa) { 
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
     }
 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getVida() {
+        return vida;
+    }
+
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+    
+    
     public void mostrar() {
         System.out.println("Jugador: " + nombre);
         System.out.println("Vida: " + vida);
         System.out.println("Ataque: " + ataque);
         System.out.println("Defensa: " + defensa);
     }
+
 }
 
