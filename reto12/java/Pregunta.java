0b22d073a1cc178308f81d9ac5b6b47aeba9ed24
Angel Calleja <angelcalleja99@alumnos.ilerna.com>
2025-05-20 14:01:57 +0200

    He añadido otra respuesta opcional al test

---
+++ reto12/java/Pregunta.java
@@ -0,0 +1,15 @@
+package reto12.java;
+
+public class Pregunta {
+    public String t;
+    public String respusta1;
+    public String respuesta2;
+    public String respuesta3;
+
+    public Pregunta(String t, String a, String b, String c) {
+        this.t = t;
+        respuesta1 = a;
+        respuesta2 = b;
+        respuesta3 = c;
+    }
+}
