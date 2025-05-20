50add0ef8c703cdf403101502396fc26550a1458
Dario <dariolopezcastillejo@alumnos.ilerna.com>
2025-05-20 14:09:26 +0200

    Añadido una variable vida_perdida

--- reto7/java/Juego.java
+++ reto7/java/Juego.java
@@ -1,17 +1,34 @@
 package reto7.java;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 public class Juego {
     public static void main(String[] args) {
+        /**
+         * Juagadores antes de tener nuevas funciones
+         */
         Jugador jugador1 = new Jugador("Arthas", 100, 30, 10);
-        Jugador jugador2 = new Jugador("Thrall", 100, 25, 15);
+        Jugador jugador2 = new Jugador("Thrall", 100, 25, 30);
 
-        if (jugador1.ataque > jugador2.defensa) {
-            jugador2.vida -= (jugador1.ataque - jugador2.defensa);
-        } else {
-            jugador1.vida -= 5;
-        }
+        int vida_perdida = jugador1.getAtaque() - jugador2.getDefensa();
 
         jugador1.mostrar();
+        
         jugador2.mostrar();
+        
+        if (jugador1.getAtaque() > jugador2.getDefensa()) {
+            System.out.println("\n El ataque ha acertado y el rival a perdido: " +vida_perdida +" puntos de vida");
+
+        }else{
+            System.out.println("\n El ataque no ha surtido efecto");
+        }
+
+       
+    
+
+
+
     }
+
+       
 }
